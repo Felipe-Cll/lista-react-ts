12a215dea1ee85d0477870ddc141338716a95a30
Felipe <FelipeCll01@outlook.com>
2022-08-27 18:12:42 -0300

    Teste

--- src/components/AddArea/index.tsx
+++ src/components/AddArea/index.tsx
@@ -10,7 +10,7 @@ export const AddArea = ({ onEnter }: Props) => {
     const [inputText, setInputText] = useState('');
 
     const handleKeyUp = (e: KeyboardEvent) => {
-        if((e.code === 'Enter' || e.code === 'NumpadEnter') && inputText !== '') {
+        if(e.code === 'Enter' && inputText !== '') {
             onEnter(inputText);
             setInputText('');
         }
